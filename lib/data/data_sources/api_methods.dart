1d06e48a178ccb67116db0d28a2c86b74f54a79a
Kapil Ramkhiladi Singh <singhkapil708@gmail.com>
2023-08-03 17:16:49 +0530

    Initial commit

---
+++ lib/data/data_sources/api_methods.dart
@@ -0,0 +1,5 @@
+const String baseUrl = 'https://develop.carerockets.com/api';
+
+class ApiMethods {
+  static const String login = '$baseUrl/login';
+}
---
+++ lib/presentation/pages/splash/splash_page.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc_clean_architecture/core/route/route_names.dart';
+import 'package:go_router/go_router.dart';
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    Future.delayed(const Duration(seconds: 5)).then((value) => context.goNamed(RouteName.home));
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SizedBox(
+        width: MediaQuery.sizeOf(context).width,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Welcome To \nBloc Clean Architecture",
+              style: Theme.of(context).textTheme.headlineLarge,
+              textAlign: TextAlign.center,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
